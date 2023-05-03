cf2b1d30fe74ad3b890e45e0bf75d20157ddff80
Shincy <shincyshnz@gmail.com>
2023-05-03 20:49:16 +0530

    Error Field issue remains and clear input field once new todo is added

--- todoapp-react/src/App.jsx
+++ todoapp-react/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import "./styles/globals.css";
 import { TodoApp } from "./components/TodoApp/TodoApp";
-import { TodoApp1 } from "./components/TodoApp/TodoApp1";
 
 function App() {
   return (
